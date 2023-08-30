547389debb4edbea50c95105ffe3b29709abedce
Jonathan Claes <115749108+JoMesss123@users.noreply.github.com>
2023-08-29 20:45:24 -0400

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {QueryClient, QueryClientProvider} from 'react-query'
+import { Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CategoryPage from './pages/CategoryPage';
 import RecipePage from './pages/RecipePage';
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <Router>
-      <Switch>
+  
+    <QueryClientProvider client={queryClient}>
+    <Routes>
+      
         <Route path="/" exact component={HomePage} />
         <Route path="/category/:categoryName" component={CategoryPage} />
         <Route path="/recipe/:recipeId" component={RecipePage} />
-      </Switch>
-    </Router>
+      
+    </Routes>
+
+    </QueryClientProvider>
+   
   );
 }
 
